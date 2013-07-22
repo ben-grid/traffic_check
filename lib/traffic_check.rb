d715e7d0d79b05b20ccebd5e3a557ae302e746c4
Ben Golden <ben@sendgrid.com>
2013-07-22 10:01:33 -0700

    fixed the app name bidness

--- lib/traffic_check.rb
+++ lib/traffic_check.rb
@@ -3,10 +3,10 @@ class TrafficCheck
   def self.check
     status = {:traffic => true, :mode => "up", :code => 200}
     
-    if File.exists?("/tmp/#{Rails.application.class.to_s.split("::").first}.maint")
+    if File.exists?("/tmp/#{Rails.application.class.to_s.split("::").first.underscore}.maint")
       status = {:traffic => false, :mode => "maintenance", :code => 404}
     end
-    if File.exists?("/tmp/#{Rails.application.class.to_s.split('::').first}.down")
+    if File.exists?("/tmp/#{Rails.application.class.to_s.split('::').first.underscore}.down")
       status = {:traffic => false, :mode => "down", :code => 500}
     end
     status
