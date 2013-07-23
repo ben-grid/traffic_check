b4694499ca007530406550255e9fc6b20a0c0b0d
Ben Golden <ben@sendgrid.com>
2013-07-23 08:40:36 -0700

    added some controller magic, still trying to get the gem to work locally

---
+++ app/controllers/traffic_check_controller.rb
@@ -0,0 +1,12 @@
+module TrafficCheck
+  class TrafficCheckController < ApplicationController  
+    respond_to :json
+    skip_before_filter :force_ssl
+
+    def index
+      @traffic_check = TrafficCheck.check
+      @status_code = @traffic_check[:code]
+      respond_with(@traffic_check, :status => @status_code)
+    end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  match "/traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
+end
