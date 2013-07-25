cf68211febf151d83e384c79e1e74a160f2f1493
Ben Golden <ben@sendgrid.com>
2013-07-25 08:46:29 -0700

    more version hoodoo

--- lib/traffic_check.rb
+++ lib/traffic_check.rb
@@ -1,18 +1,18 @@
 require 'traffic_check/version'
 
-class TrafficCheck
+module TrafficCheck
+  class TrafficCheck  
+    def self.check
+      status = {:traffic => true, :mode => "up", :code => 200}
+      app_name = "app"
     
-  def self.check
-    status = {:traffic => true, :mode => "up", :code => 200}
-    app_name = "app"
-    
-    if File.exists?("/tmp/#{app_name}.maint")
-      status = {:traffic => false, :mode => "maintenance", :code => 404}
-    end
-    if File.exists?("/tmp/#{app_name}.down")
-      status = {:traffic => false, :mode => "down", :code => 500}
-    end
-    status
-  end  
-  
+      if File.exists?("/tmp/#{app_name}.maint")
+        status = {:traffic => false, :mode => "maintenance", :code => 404}
+      end
+      if File.exists?("/tmp/#{app_name}.down")
+        status = {:traffic => false, :mode => "down", :code => 500}
+      end
+      status
+    end  
+  end
 end
