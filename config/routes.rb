6a76edb14fbeafa457f3dbf0374feeb897ca88fe
Ben Golden <ben@sendgrid.com>
2013-07-25 20:31:47 -0700

    version

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 require 'traffic_check/version'
 
 WebRails::Application.routes.draw do
-  namespace :kasi, :module => 'admin', :as => 'admin' do
-    match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
-  end
+  match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
 end
