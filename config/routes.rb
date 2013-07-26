01fe9c8ed39959891749fa578e9ede802b59ede7
Ben Golden <ben@sendgrid.com>
2013-07-25 20:03:15 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 require 'traffic_check/version'
 
 Rails.application.routes.draw do
-  match "/traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
+  map.root :controller => "traffic_check", :action => "index"  
 end
