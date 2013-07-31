d5622152ca0c333b83f3a6f0bea04109c9effc2d
Ben Golden <ben@sendgrid.com>
2013-07-31 14:27:31 -0700

    one more time

--- lib/rails/routes.rb
+++ lib/rails/routes.rb
@@ -2,7 +2,7 @@ module ActionDispatch::Routing
   class Mapper
     # Includes mount_traffic_check method for routes
     def mount_traffic_check
-      match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
+      match "traffic_check" => "traffic_check/traffic_check#index", :via => :get, defaults: {format: 'json'}
       mount TrafficCheck::Engine => "/", :as => "traffic_check_engine"
     end
   end
