4376d21221ad23aef2801ba83e72cf82e8457721
Ben Golden <ben@sendgrid.com>
2013-07-31 13:19:43 -0700

    merr

--- lib/traffic_check/config/routes.rb
+++ lib/traffic_check/config/routes.rb
@@ -1,5 +1,7 @@
+require 'rails'
 require 'traffic_check/version'
-
-Rails.application.routes.draw do
-  match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
+module TrafficCheck
+  Rails.application.routes.draw do
+    match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
+  end
 end
