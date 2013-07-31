fd9d4aeaf3912629a66035e593e3182691a18eab
Ben Golden <ben@sendgrid.com>
2013-07-31 12:54:03 -0700

    trying something

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,4 @@ require 'traffic_check/version'
 WebRails::Application.routes.draw do
   match "traffic_check" => "traffic_check#index", :via => :get, defaults: {format: 'json'}
 end
+ActiveSupport.on_load(:action_controller) { WebRails::Application.routes.finalize! }
